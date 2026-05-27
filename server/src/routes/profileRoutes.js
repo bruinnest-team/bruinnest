e62ed0a3fef155c0551de6e2e8b88e4e4c22171c
Renoir-J <jeffreyqrj@gmail.com>
2026-05-27 02:04:34 -0700

    feat: add profile read and update endpoints

--- server/src/routes/profileRoutes.js
+++ server/src/routes/profileRoutes.js
@@ -5,5 +5,7 @@ const profileController = require("../controllers/profileController");
 const requireAuth = require("../middlewares/requireAuth");
 
 router.post("/profile", requireAuth, profileController.createProfile);
+router.get("/profile/me", requireAuth, profileController.getMyProfile);
+router.put("/profile/me", requireAuth, profileController.updateProfile);
 
 module.exports = router;
