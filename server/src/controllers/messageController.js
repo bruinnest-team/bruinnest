2f25f983299618aa81c00147c45a10320f1ee3d5
Yinlong Shen <yinlong06@g.ucla.edu>
2026-05-27 22:11:39 -0700

    fix: align message controller error handling

--- server/src/controllers/messageController.js
+++ server/src/controllers/messageController.js
@@ -1,47 +1,74 @@
 const messageService = require("../services/messageService");
+const { success } = require("../utils/apiResponse");
 
-function respond(res, data) {
-  res.json({ success: true, data });
+async function createConversation(req, res, next) {
+  try {
+    const result = await messageService.createOrGetConversation(
+      req.session.userId,
+      req.body.targetUserId
+    );
+    return success(res, result);
+  } catch (err) {
+    next(err);
+  }
 }
 
-function createConversation(req, res) {
-  const result = messageService.createOrGetConversation(
-    req.session.userId,
-    req.body.targetUserId
-  );
-  respond(res, result);
+async function listConversations(req, res, next) {
+  try {
+    const result = await messageService.listConversations(req.session.userId);
+    return success(res, result);
+  } catch (err) {
+    next(err);
+  }
 }
 
-function listConversations(req, res) {
-  respond(res, messageService.listConversations(req.session.userId));
+async function getMessages(req, res, next) {
+  try {
+    const result = await messageService.getMessages(
+      req.session.userId,
+      req.params.conversationId,
+      req.query.afterMessageId
+    );
+    return success(res, result);
+  } catch (err) {
+    next(err);
+  }
 }
 
-function getMessages(req, res) {
-  const result = messageService.getMessages(
-    req.session.userId,
-    req.params.conversationId,
-    req.query.afterMessageId
-  );
-  respond(res, result);
+async function sendMessage(req, res, next) {
+  try {
+    const { conversationId, body } = req.body;
+    const result = await messageService.sendMessage(
+      req.session.userId,
+      conversationId,
+      body
+    );
+    return success(res, result);
+  } catch (err) {
+    next(err);
+  }
 }
 
-function sendMessage(req, res) {
-  const { conversationId, body } = req.body;
-  const result = messageService.sendMessage(req.session.userId, conversationId, body);
-  respond(res, result);
+async function markRead(req, res, next) {
+  try {
+    const result = await messageService.markConversationRead(
+      req.session.userId,
+      req.params.conversationId,
+      req.body.lastReadMessageId
+    );
+    return success(res, result);
+  } catch (err) {
+    next(err);
+  }
 }
 
-function markRead(req, res) {
-  const result = messageService.markConversationRead(
-    req.session.userId,
-    req.params.conversationId,
-    req.body.lastReadMessageId
-  );
-  respond(res, result);
-}
-
-function unreadSummary(req, res) {
-  respond(res, messageService.getUnreadSummary(req.session.userId));
+async function unreadSummary(req, res, next) {
+  try {
+    const result = await messageService.getUnreadSummary(req.session.userId);
+    return success(res, result);
+  } catch (err) {
+    next(err);
+  }
 }
 
 module.exports = {
